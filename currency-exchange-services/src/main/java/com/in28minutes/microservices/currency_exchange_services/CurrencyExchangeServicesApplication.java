4a27a6528a4b67bab9c1b002b1e9a5f1f6017404
hussien <hussien0792@gmail.com>
2026-06-20 12:57:30 +0200

    Add currency exchange to project

---
+++ currency-exchange-services/src/main/java/com/in28minutes/microservices/currency_exchange_services/CurrencyExchangeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currency_exchange_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServicesApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-services/src/test/java/com/in28minutes/microservices/currency_exchange_services/CurrencyExchangeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currency_exchange_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

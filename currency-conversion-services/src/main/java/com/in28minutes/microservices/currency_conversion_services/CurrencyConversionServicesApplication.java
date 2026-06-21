1f6310a17725ea530e4c002de1c24f614d309fe2
hussien <hussien0792@gmail.com>
2026-06-21 12:56:40 +0200

    Add CurrencyConversion branch to project

---
+++ currency-conversion-services/src/main/java/com/in28minutes/microservices/currency_conversion_services/CurrencyConversionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currency_conversion_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServicesApplication.class, args);
+	}
+
+}

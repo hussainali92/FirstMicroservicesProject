f1b149ed9215862982f56e1df07373fc4c54ee81
hussien <hussien0792@gmail.com>
2026-06-24 19:01:19 +0200

    Add api-gateway to project and configure LoggingFilter

---
+++ api-gateway/src/main/java/com/in28mintues/microservices/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,31 @@
+package com.in28mintues.microservices.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/currency-exchange/**")
+						.uri("lb://CURRENCY-EXCHANGE"))
+				.route(p -> p
+						.path("/currency-conversion/**")
+						.uri("lb://CURRENCY-CONVERSION"))
+				.route(p -> p
+						.path("/currency-conversion-feign/**")
+						.uri("lb://CURRENCY-CONVERSION"))
+				.build();
+	}
+}
+
+

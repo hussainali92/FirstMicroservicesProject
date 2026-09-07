471b7cce82190c6838f77e3c600560ed63d7897c
hussien <hussien0792@gmail.com>
2026-09-07 19:08:01 +0200

    add eureka config to properties and main class to conversion

--- currency-conversion-services/src/main/java/com/in28minutes/microservices/currency_conversion_services/CurrencyConversionServicesApplication.java
+++ currency-conversion-services/src/main/java/com/in28minutes/microservices/currency_conversion_services/CurrencyConversionServicesApplication.java
@@ -2,10 +2,12 @@ package com.in28minutes.microservices.currency_conversion_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyConversionServicesApplication {
 
 	public static void main(String[] args) {
